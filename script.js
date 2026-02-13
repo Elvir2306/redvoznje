d42738260e4c0edd27228f3d3ddf366604c1dd2e
Elvir2306 <elvir.imad@gmail.com>
2026-02-13 18:01:04 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function updateClock() {
     let hours = now.getHours();
     const minutes = String(now.getMinutes()).padStart(2, '0');
 	const seconds = String(now.getSeconds()).padStart(2, '0');
-    const ampm = hours >= 12 ? '' : '';
+	const ampm = hours >= 12 ? '' : '';
     hours = hours % 24 || 12;
     
     document.getElementById('time').textContent = `${hours}:${minutes}:${seconds}`;
@@ -34,7 +34,7 @@ function showResults() {
         resultsDiv.innerHTML = `
             <div class="welcome-message">
                 <i class="fas fa-compass"></i>
-                <p>Odaberi željenu destinaciju</p>
+                <p>Odaberi mjesto polaska i odredište</p>
             </div>`;
         return;
     }
